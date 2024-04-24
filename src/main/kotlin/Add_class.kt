5f4527984e846a8ec4ebaa36b9131efa4209d0f8
gooddle <vptl6559@gmail.com>
2024-04-24 12:22:32 +0900

    "Cal"

--- src/main/kotlin/Add_class.kt
+++ src/main/kotlin/Add_class.kt
@@ -2,4 +2,4 @@ class AddOperation(numDou1: Double, numDou2: Double) : Cal(numDou1, numDou2) {
 
     fun operateAdd(): Double = add()
     var resultAdd = println("결과값 : ${numDou1}+${numDou2}  입니다.")
-}
+}//dd
