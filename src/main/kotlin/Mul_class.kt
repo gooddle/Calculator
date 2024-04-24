e4d760f0dce539f1f82cf14d694cd6c722b1b87a
gooddle <vptl6559@gmail.com>
2024-04-24 12:21:26 +0900

    "Cal"

---
+++ src/main/kotlin/Mul_class.kt
@@ -0,0 +1,5 @@
+class MulOperation(numDou1: Double, numDou2: Double) : Cal(numDou1, numDou2) {
+
+    fun operateMul(): Double = mul()
+    var resultMul = println("결과값 : ${numDou1}*${numDou2}  입니다.")
+}
