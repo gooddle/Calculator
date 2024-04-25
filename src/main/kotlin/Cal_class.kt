254baea9ffbb96537dabbe615888d63436711217
gooddle <vptl6559@gmail.com>
2024-04-25 19:13:00 +0900

    파일명 변경

--- src/main/kotlin/Cal_class.kt
+++
@@ -1,7 +0,0 @@
-package org.example
-
-class Cal {
-    fun operate(operation: AbstractOperation, numDou1: Double, numDou2: Double): Double {
-        return operation.operate(numDou1, numDou2)
-    }
-}
